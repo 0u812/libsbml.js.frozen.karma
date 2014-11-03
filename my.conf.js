e288676d8f3802db7058285ac038e3cc2faacefc
0u812 <0u812@github.com>
2014-11-03 12:42:46 -0800

    serve decay model

--- my.conf.js
+++ my.conf.js
@@ -18,7 +18,8 @@ module.exports = function(config) {
       {pattern: 'pre.js', watched: true, served: true, included: true},
       {pattern: 'libsbml.js', watched: true, served: true, included: true},
       {pattern: 'libsbml.js.mem', watched: false, served: true, included: false},
-      {pattern: 'libsbml-basic.js', watched: true, served: true, included: true}
+      {pattern: 'libsbml-basic.js', watched: true, served: true, included: true},
+      {pattern: 'models/decayModel.xml', watched: false, served: true, included: false}
     ],
 
     proxies: {
