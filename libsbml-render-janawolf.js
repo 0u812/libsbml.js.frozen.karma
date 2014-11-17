fdfe21c9d408b16bc46ce2a657eb94d08ecd253e
0u812 <0u812@github.com>
2014-11-17 14:17:16 -0800

    jana wolf glyco render

---
+++ libsbml-render-janawolf.js
@@ -0,0 +1,45 @@
+var doc6;
+var ready6 = false;
+
+describe("Test of import coverage", function() {
+  ready6 = false;
+  // load the model asynchronously
+  libsbml.load('models/GlycolysisOriginal.xml', function(result) {
+    doc6 = result.doc;
+    ready6 = true;
+  });
+
+  it('loads SBML', function() {
+    waitsFor(function() {
+        return ready6;
+      }, 'the model to load', 10000);
+
+    runs( function() {
+      // read with no errors
+      expect(doc6.getNumErrors()).toEqual(0);
+      doc6.errors.forEach(function(x) {
+        console.log(x.getMessage());
+      });
+
+      // check reactions
+      expect(doc6.getModel().reactions.length).toEqual(11);
+
+      // check plugins
+      var caps = new libsbml.Capabilities();
+      expect(caps.isLayoutSupported()).toEqual(true);
+      expect(caps.isRenderSupported()).toEqual(true);
+
+      expect(doc6.getModel().hasPlugin('layout')).toEqual(true);
+      var lplugin6 = doc6.getModel().findPlugin('layout');
+      expect(lplugin6.getPackageName()).toEqual('layout');
+      var layoutlplugin6 = lplugin6.asLayout();
+      expect(layoutlplugin6.layouts.length).toEqual(1);
+
+      var layout6 = layoutlplugin6.layouts[0];
+    });
+  });
+
+  it('cleans up resources', function() {
+    libsbml.destroy(doc6);
+  });
+});
